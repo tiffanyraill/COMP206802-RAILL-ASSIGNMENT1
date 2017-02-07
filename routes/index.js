9d787456cb7a0caf73010ef5bebea0e76821e250
Tiffany Raill <tiffany.raill@mygeorgian.ca>
2017-02-07 18:29:00 -0500

    Added funtionality to header

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Portfolio',
+  message: 'This is what I have been doing for the Past 3 Years!'
+  });
 });
 
+/*GET aboutMe */
+router.get('/aboutMe', function(req, res, next) {
+    //load the aboutMe.ejs view
+    res.render('aboutMe');
+});
+/*GET contactMe */
+router.get('/contactMe', function(req, res, next) {
+    //load the contactMe.ejs view
+    res.render('contactMe');
+});
+/*GET Projects */
+router.get('/Projects', function(req, res, next) {
+    //load the Projects.ejs view
+    res.render('Projects');
+});
+/*GET Services */
+router.get('/Services', function(req, res, next) {
+    //load the Services .ejs view
+    res.render('Services');
+});
 module.exports = router;
